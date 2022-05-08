08df229da949be891d6d56ad5c7dcf47a3b9c455
Sveta <sveta.zheleznova.03@mail.ru>
2022-05-08 20:37:50 +0300

    del unnecessary things

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-import datetime
-import time
-
-#datetime object
-dt = datetime.datetime.now()
-a=0
-print("Datetime object =", dt)
-
-# printing in dd/mm/YY H:M:S format using strftime()
-
-print(dt.strftime("%H:%M:%S"))
-time.sleep(3)
-
-print(float(datetime.datetime.now().strftime("%H:%M:%S")) - float(dt.strftime("%H:%M:%S")))
+# in process
