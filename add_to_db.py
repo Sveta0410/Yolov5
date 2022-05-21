ce348335a703767beaa1f32689e825a08d864b01
Sveta <sveta.zheleznova.03@mail.ru>
2022-05-21 17:45:44 +0300

    now ALL tests are working

--- add_to_db.py
+++ add_to_db.py
@@ -1,26 +1,19 @@
-add = []
-
-
 def add_to_db(inf, all_id):
+    add = []
     for i in all_id:
-        print(inf['accuracy%s' % i])
-        print(inf['time%s' % i])
         inf['accuracy%s' % i] = round(inf['accuracy%s' % i] / (inf['time%s' % i]), 2)
-        print(inf['accuracy%s' % i])
         number = str(i)
         name = str(inf['name%s' % i])
         accuracy = str(inf['accuracy%s' % i])
         time_ent = str(inf['time_ent_%s' % i])
         time_out = str(inf['time_out_%s' % i])
-        print(number, name, time_ent, time_out, accuracy)
         add.append([number, name, time_ent, time_out, accuracy])
-        print(add)
 
         try:
             cursor.execute('INSERT INTO main.info (number, fight_fall, time_ent, time_out, average_accuracy) '
                               'VALUES (%s, %s, %s, %s, %s);', (number, name, time_ent, time_out, accuracy))
             conn.commit()
         except:
-            pass
+            continue
 
     return add
--- add_to_db_test.py
+++ add_to_db_test.py
@@ -7,7 +7,6 @@ def test_insert_1():
            'time_ent_2': '10:57:08', 'time_out_2': '10:58:02', 'name3': 'fight', 'accuracy3': 1.45782, 'time3': 2,
            'time_ent_3': '10:57:21', 'time_out_3': '10:57:21'}
     all_id = [1, 2, 3]
-    add_to_db(inf, all_id)
     assert add_to_db(inf, all_id)[0] == ['1', 'fight', '10:57:08', '10:57:19', '0.84']
 
 
@@ -17,11 +16,21 @@ def test_insert_2():
            'time_ent_2': '10:57:08', 'time_out_2': '10:58:02', 'name3': 'fight', 'accuracy3': 1.45782, 'time3': 2,
            'time_ent_3': '10:57:21', 'time_out_3': '10:57:21'}
     all_id = [1, 2, 3]
-    add_to_db(inf, all_id)
     assert add_to_db(inf, all_id)[1] == ['2', 'fight', '10:57:08', '10:58:02', '0.82']
 
 
 def test_insert_3():
+    inf = {'name1': 'fight', 'accuracy1': 13.397889999999999, 'time1': 16, 'time_ent_1': '10:57:08',
+           'time_out_1': '10:57:19', 'name2': 'fight', 'accuracy2': 74.86325999999995, 'time2': 91,
+           'time_ent_2': '10:57:08', 'time_out_2': '10:58:02', 'name3': 'fight', 'accuracy3': 1.45782, 'time3': 2,
+           'time_ent_3': '10:57:21', 'time_out_3': '10:57:21', 'name4': 'fight', 'accuracy4': 6.755979999999999,
+           'time4': 9, 'time_ent_4': '10:57:24', 'time_out_4': '10:57:29', 'name7': 'fight',
+           'accuracy7': 15.783180000000003, 'time7': 21, 'time_ent_7': '10:57:41', 'time_out_7': '10:57:54'}
+    all_id = [1, 2, 3, 4, 7]
+    assert add_to_db(inf, all_id)[4] == ['7', 'fight', '10:57:41', '10:57:54', '0.75']
+
+
+def test_insert_4():
     inf = {'name1': 'fight', 'accuracy1': 13.397889999999999, 'time1': 16, 'time_ent_1': '10:57:08',
            'time_out_1': '10:57:19', 'name2': 'fight', 'accuracy2': 74.86325999999995, 'time2': 91,
            'time_ent_2': '10:57:08', 'time_out_2': '10:58:02', 'name3': 'fight', 'accuracy3': 1.45782, 'time3': 2,
@@ -34,7 +43,23 @@ def test_insert_3():
            'time_ent_12': '10:58:27', 'time_out_12': '10:58:28', 'name13': 'fight', 'accuracy13': 7.4717899999999995,
            'time13': 10, 'time_ent_13': '10:58:33', 'time_out_13': '10:58:39', 'name15': 'fight', 'accuracy15': 2.15566,
            'time15': 3, 'time_ent_15': '10:58:44', 'time_out_15': '10:58:45'}
-    all_id = [1, 2, 3, 4, 7, 9, 11, 12, 13]
-    add_to_db(inf, all_id)
-    assert add_to_db(inf, all_id)[0] == ['1', 'fight', '10:57:08', '10:57:19', '0.84']
-    assert add_to_db(inf, all_id)[1] == ['2', 'fight', '10:57:08', '10:58:02', '0.82']
+    all_id = [1, 2, 3, 4, 7, 9, 11, 12, 13, 15]
+    assert add_to_db(inf, all_id)[8] == ['13', 'fight', '10:58:33', '10:58:39', '0.75']
+
+
+def test_insert_4():
+    add = []
+    inf = {'name1': 'fight', 'accuracy1': 13.397889999999999, 'time1': 16, 'time_ent_1': '10:57:08',
+           'time_out_1': '10:57:19', 'name2': 'fight', 'accuracy2': 74.86325999999995, 'time2': 91,
+           'time_ent_2': '10:57:08', 'time_out_2': '10:58:02', 'name3': 'fight', 'accuracy3': 1.45782, 'time3': 2,
+           'time_ent_3': '10:57:21', 'time_out_3': '10:57:21', 'name4': 'fight', 'accuracy4': 6.755979999999999,
+           'time4': 9, 'time_ent_4': '10:57:24', 'time_out_4': '10:57:29', 'name7': 'fight',
+           'accuracy7': 15.783180000000003, 'time7': 21, 'time_ent_7': '10:57:41', 'time_out_7': '10:57:54',
+           'name9': 'fight', 'accuracy9': 19.90835, 'time9': 26, 'time_ent_9': '10:58:00', 'time_out_9': '10:58:16',
+           'name11': 'fight', 'accuracy11': 53.42228000000002, 'time11': 70, 'time_ent_11': '10:58:07',
+           'time_out_11': '10:58:45', 'name12': 'fight', 'accuracy12': 1.3086600000000002, 'time12': 2,
+           'time_ent_12': '10:58:27', 'time_out_12': '10:58:28', 'name13': 'fight', 'accuracy13': 7.4717899999999995,
+           'time13': 10, 'time_ent_13': '10:58:33', 'time_out_13': '10:58:39', 'name15': 'fight', 'accuracy15': 2.15566,
+           'time15': 3, 'time_ent_15': '10:58:44', 'time_out_15': '10:58:45'}
+    all_id = [1, 2, 3, 4, 7, 9, 11, 12, 13, 15]
+    assert add_to_db(inf, all_id)[9] == ['15', 'fight', '10:58:44', '10:58:45', '0.72']
