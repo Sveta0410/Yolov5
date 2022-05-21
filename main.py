ce6e88260c31ab6a284734cb91db2a1b2cecb4ad
Sveta <sveta.zheleznova.03@mail.ru>
2022-05-21 23:55:01 +0300

    backend

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-# everything in track.py
+import os
+from http.server import HTTPServer, CGIHTTPRequestHandler
+os.chdir('gstr')
+# Create server object listening the port 8080
+server_object = HTTPServer(server_address=('', 8080), RequestHandlerClass=CGIHTTPRequestHandler)
+# Start the web server
+server_object.serve_forever()
