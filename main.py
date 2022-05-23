9764f0cc8f344e9f61d4daa5354d5b6fcc645bb9
Sveta <sveta.zheleznova.03@mail.ru>
2022-05-23 19:46:36 +0300

    i dont know what im doing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from http.server import HTTPServer, CGIHTTPRequestHandler
-os.chdir('gstr')
+#  os.chdir('gstr')
+os.chdir('templates')
 # Create server object listening the port 8080
 server_object = HTTPServer(server_address=('', 8080), RequestHandlerClass=CGIHTTPRequestHandler)
 # Start the web server
